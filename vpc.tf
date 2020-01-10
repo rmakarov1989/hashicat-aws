985eb209bf6bd42c1d43af346f05c0e573ccf8fd
Roman <rmakarov@fb.com>
2020-01-10 00:49:37 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,16 @@
+//--------------------------------------------------------------------
+// Modules
 module "vpc" {
   source  = "app.terraform.io/fb/vpc/aws"
   version = "2.21.0"
 
-  name = "gaur-vpc"
-  cidr = "10.20.0.0/16"
-  
-  tags = {
-   departmnet = 'sky'
-   Billable = 'yes'
+  cidr = "10.20.0.1/16"
+  database_subnet_assign_ipv6_address_on_creation = "false"
+  elasticache_subnet_assign_ipv6_address_on_creation = "false"
+  enable_classiclink = "false"
+  enable_classiclink_dns_support = "false"
+  intra_subnet_assign_ipv6_address_on_creation = "false"
+  private_subnet_assign_ipv6_address_on_creation = "false"
+  public_subnet_assign_ipv6_address_on_creation = "false"
+  redshift_subnet_assign_ipv6_address_on_creation = "false"
 }
-}
-
